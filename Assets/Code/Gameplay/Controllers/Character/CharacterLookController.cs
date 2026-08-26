19522b2157cc5076782f1494e3da0ec266f13412
Fricodev <fede97cem@gmail.com>
2026-08-26 19:46:58 -0300

    [A] Added basic FPS controller to the game

---
+++ Assets/Code/Gameplay/Controllers/Character/CharacterLookController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using CorePatterns.ServiceLocator;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Rulebound
+{
+    public class CharacterLookController : MonoBehaviour, ICharacterLookController
+    {
+         
+        [Header("Input")]
+        [SerializeField] private InputActionSO lookActionSO;
+
+        [Header("References")] 
+        [SerializeField] private Rigidbody rigidBody;
+        [SerializeField] private Transform bodyTransform;
+        [SerializeField] private Transform cameraPivot;   
+ 
+        [Header("Sensitivity")]
+        [SerializeField] private float mouseSensitivity = 0.15f;
+        [SerializeField] private float gamepadSensitivity = 120f;
+        [SerializeField] private bool invertY;
+ 
+        [Header("Pitch Clamp")]
+        [SerializeField] private float minPitch = -85f;
+        [SerializeField] private float maxPitch = 85f;
+ 
+        [Header("Headbob")]
+        [SerializeField] private HeadbobSettings headbob = new();
+        
+        private float _yaw;
+        private float _pitch;
+        private Vector3 _cameraRestLocalPos;
+        private float _bobTimer;
+        private bool _cursorLocked = true;
+
+        private InputAction _lookAction;
+        private bool _isEnabled;
+        private ICharacterMovementController _characterMovementController;
+
+        private void Awake()
+        {
+            InstallService();
+        }
+
+        private void InstallService()
+        {
+            if (cameraPivot)
+            {
+                _cameraRestLocalPos = cameraPivot.localPosition;
+            }
+
+            _yaw = bodyTransform ? bodyTransform.eulerAngles.y : 0f;
+
+            _lookAction = lookActionSO.GetInputAction();
+            
+            SetCursorLocked(true);
+            
+            ServiceLocator.RegisterService<ICharacterLookController>(this);
+        }
+
+        private void Start()
+        {
+            _characterMovementController = ServiceLocator.GetService<ICharacterMovementController>();
+            EnableController();
+        }
+
+        private void LateUpdate()
+        {
+            if (!_isEnabled)
+            {
+                return;
+            }
+            
+            ApplyLook();
+            ApplyHeadbob();
+        }
+
+        private void ApplyLook()
+        {
+            Vector2 delta = _lookAction.ReadValue<Vector2>();
+
+            if (delta.sqrMagnitude < 0.0001f)
+            {
+                return;
+            }
+
+            bool usingGamepad = _lookAction.activeControl != null && _lookAction.activeControl.device is Gamepad;
+            float sens = usingGamepad ? gamepadSensitivity * Time.deltaTime : mouseSensitivity;
+
+            _yaw += delta.x * sens;
+            _pitch += (invertY ? delta.y : -delta.y) * sens;
+            _pitch = Mathf.Clamp(_pitch, minPitch, maxPitch);
+
+            if (bodyTransform)
+            {
+                bodyTransform.rotation = Quaternion.Euler(0f, _yaw, 0f);
+            }
+
+            if (cameraPivot)
+            {
+                cameraPivot.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
+            }
+        }
+ 
+        private void ApplyHeadbob()
+        {
+            if (!headbob.enabled || !cameraPivot)
+                return;
+
+            if (_characterMovementController.IsMoving)
+            {
+                _bobTimer += Time.deltaTime * headbob.frequency;
+
+                float bobY = Mathf.Sin(_bobTimer) * headbob.amplitude;
+                float bobX = Mathf.Cos(_bobTimer * 0.5f) * headbob.amplitude;
+
+                Vector3 targetPosition = _cameraRestLocalPos + new Vector3(bobX, bobY, 0f);
+
+                cameraPivot.localPosition = Vector3.Lerp(
+                    cameraPivot.localPosition,
+                    targetPosition,
+                    headbob.smoothing * Time.deltaTime);
+            }
+            else
+            {
+                cameraPivot.localPosition = Vector3.Lerp(
+                    cameraPivot.localPosition,
+                    _cameraRestLocalPos,
+                    headbob.smoothing * Time.deltaTime);
+            }
+        }
+
+        public void EnableController()
+        {
+            _isEnabled = true;
+        }
+
+        public void DisableController()
+        {
+            _isEnabled = false;
+        }
+
+        public void SetCursorLocked(bool locked)
+        {
+            _cursorLocked = locked;
+            Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = !locked;
+        }
+    }
+ 
+    [Serializable]
+    public class HeadbobSettings
+    {
+        public bool enabled = true;
+        public float frequency = 8f;
+        public float amplitude = 0.05f;
+        public float smoothing = 10f;
+    }
+
+    public interface ICharacterLookController
+    {
+        public void EnableController();
+        public void DisableController();
+        void SetCursorLocked(bool locked);
+    }
+}
---
+++ Assets/Code/Gameplay/Controllers/Character/CharacterMovementController.cs
@@ -0,0 +1,176 @@
+﻿using CorePatterns.ServiceLocator;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Rulebound
+{
+    public class CharacterMovementController : MonoBehaviour, ICharacterMovementController
+    {
+        [Header("References")]
+        [SerializeField] private Rigidbody rigidBody;
+        [SerializeField] private CapsuleCollider capsuleCollider;
+
+        [Header("Input Actions")]
+        [SerializeField] private InputActionSO moveActionSO;
+        [SerializeField] private InputActionSO jumpActionSO;
+        [SerializeField] private InputActionSO sprintActionSO;
+
+        [Header("Movement")]
+        [SerializeField] private float walkSpeed = 5f;
+        [SerializeField] private float sprintMultiplier = 2.5f;
+        
+        [Header("Jumping")]
+        [SerializeField] private float jumpForce = 5f;
+        [SerializeField] private float jumpHeight = 3f;
+        [SerializeField] private float gravityMultiplier = 2.5f;
+
+        private InputAction _moveAction;
+        private InputAction _jumpAction;
+        private InputAction _sprintAction;
+
+        private Vector2 _movementInput;
+        private bool _isMoving;
+        private bool _isSprinting;
+        
+        private bool _isGrounded;
+
+        private Coroutine _crouchRoutine;
+        private bool _isEnabled;
+
+        public bool IsMoving => _isMoving;
+
+        private void Awake()
+        {
+            InstallService();
+        }
+
+        private void Update()
+        {
+            if (!_isEnabled)
+                return;
+            
+            UpdateMovementState();
+        }
+
+        private void FixedUpdate()
+        {
+            if (!_isEnabled)
+                return;
+
+            Move();
+            UpdateGrounded();
+            ApplyGravity();
+        }
+
+        private void InstallService()
+        {
+            ServiceLocator.RegisterService<ICharacterMovementController>(this);
+
+            if (!rigidBody)
+            {
+                rigidBody = GetComponent<Rigidbody>();
+            }
+
+            rigidBody.freezeRotation = true;
+            rigidBody.useGravity = true;
+
+            _moveAction = moveActionSO.GetInputAction();
+            _jumpAction = jumpActionSO.GetInputAction();
+            _sprintAction = sprintActionSO.GetInputAction();
+
+            _moveAction.performed += HandleMovement;
+            _moveAction.canceled += HandleMovement;
+
+            _sprintAction.performed += HandleSprint;
+            _sprintAction.canceled += HandleSprint;
+            
+            _jumpAction.performed += OnJump;
+            
+            EnableController();
+        }
+
+        private void HandleSprint(InputAction.CallbackContext context)
+        {
+            _isSprinting = context.ReadValue<float>() > 0;
+        }
+
+        private void OnDestroy()
+        {
+            _moveAction.performed -= HandleMovement;
+            _moveAction.canceled -= HandleMovement;
+            _jumpAction.performed -= OnJump;
+            
+            _sprintAction.performed -= HandleSprint;
+            _sprintAction.canceled -= HandleSprint;
+        }
+
+        private void Move()
+        {
+            float speed = GetMovementSpeed();
+            Vector3 inputDirection = new Vector3(_movementInput.x, 0, _movementInput.y).normalized;
+            Vector3 move = transform.TransformDirection(inputDirection) * (speed * Time.fixedDeltaTime);
+            rigidBody.MovePosition(rigidBody.position + move);
+        }
+
+        private float GetMovementSpeed()
+        {
+            return _isSprinting ? walkSpeed * sprintMultiplier : walkSpeed;
+        }
+
+        private void UpdateMovementState()
+        {
+            _isMoving = _movementInput.sqrMagnitude > 0.01f;
+        }
+
+        private void UpdateGrounded()
+        {
+            Vector3 origin = transform.position + Vector3.up * 0.1f;
+            _isGrounded = Physics.Raycast(origin, Vector3.down, capsuleCollider.height / 2f + 0.15f);
+        }
+
+        private void HandleMovement(InputAction.CallbackContext context)
+        {
+            _movementInput = context.ReadValue<Vector2>();
+        }
+
+        private void OnJump(InputAction.CallbackContext context)
+        {
+            if (!_isGrounded)
+            {
+                return;
+            }
+            
+            Vector3 velocity = rigidBody.linearVelocity;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+
+            rigidBody.linearVelocity = velocity;
+        }
+        
+        private void ApplyGravity()
+        {
+            if (_isGrounded)
+            {
+                return;
+            }
+
+            rigidBody.AddForce(Physics.gravity * (gravityMultiplier - 1f), ForceMode.Acceleration);
+        }
+        
+        public void EnableController()
+        {
+            _isEnabled = true;
+        }
+
+        public void DisableController()
+        {
+            _isEnabled = false;
+        }
+    }
+
+    public interface ICharacterMovementController
+    {
+        bool IsMoving { get; }
+        void EnableController();
+        void DisableController();
+    }
+}
